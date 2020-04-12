41c86c45acf340a6b0ad88336d0de47e74d07f6a
Francesco <francesco.cameli95@gmail.com>
2020-04-12 01:12:29 +0100

    Supernova lock + read + better safety

--- omnicollider_lang/omnicollider_lang/lib/buffer/omnicollider_buffer.nim
+++ omnicollider_lang/omnicollider_lang/lib/buffer/omnicollider_buffer.nim
@@ -37,6 +37,7 @@ proc get_buffer_SC(buffer_SCWorld : pointer, fbufnum : cfloat) : pointer {.impor
 proc get_sc_world*() : pointer {.importc, cdecl.}
 
 when defined(multithreadBuffers):
+    proc lock_buffer_SC  (buf : pointer) : void {.importc, cdecl.}
     proc unlock_buffer_SC(buf : pointer) : void {.importc, cdecl.}
 
 proc get_float_value_buffer_SC(buf : pointer, index : clong, channel : clong) : cfloat {.importc, cdecl.}
@@ -51,7 +52,7 @@ proc get_channels_buffer_SC(buf : pointer) : cint {.importc, cdecl.}
 
 proc get_samplerate_buffer_SC(buf : pointer) : cdouble {.importc, cdecl.}
 
-proc get_sampledur_buffer_SC(buf : pointer) : cdouble {.importc, cdecl.}
+#proc get_sampledur_buffer_SC(buf : pointer) : cdouble {.importc, cdecl.}
 
 type
     Buffer_obj = object
@@ -59,6 +60,11 @@ type
         snd_buf    : pointer
         bufnum     : float32
         input_num* : int       #need to export it in order to be retrieved with the ins_Nim[buffer.input_num][0] syntax for get_buffer.
+        length     : int
+        size       : int
+        chans      : int
+        samplerate : float
+        #sampledur  : float
 
     Buffer* = ptr Buffer_obj
 
@@ -78,6 +84,11 @@ proc innerInit*[S : SomeInteger](obj_type : typedesc[Buffer], input_num : S, omn
 
     #1 should be 0, 2 1, 3 2, etc... 32 31
     result.input_num = int(input_num) - int(1)
+
+    result.length = 0
+    result.size = 0
+    result.chans = 0
+    result.samplerate = 0.0
 
     #If these checks fail set to sc_world to nil, which will invalidate the Buffer.
     #result.input_num is needed for get_buffer(buffer, ins[0][0), as 1 is the minimum number for ins, for now...
@@ -107,20 +118,52 @@ proc get_buffer*(buffer : Buffer, fbufnum : float32) : bool {.inline.} =
     if bufnum < 0.0:
         bufnum = 0.0
 
-    #Update buffer pointer only with a new buffer number as input
-    if buffer.bufnum != bufnum:
-        buffer.bufnum  = bufnum
-        buffer.snd_buf = get_buffer_SC(buffer.sc_world, cfloat(bufnum))
-    
-    if isNil(buffer.snd_buf):
-        return false
+    #supernova
+    when defined(multithreadBuffers):
+        #If same buffer number, just lock it
+        if buffer.bufnum == bufnum:
+            if isNil(buffer.snd_buf):
+                return false
+            
+            lock_buffer_SC(buffer.snd_buf)
+        
+        #Retrieve and lock the new buffer
+        else:
+            #When supernova defined, get_buffer_SC will also lock the buffer!
+            buffer.snd_buf = get_buffer_SC(buffer.sc_world, cfloat(bufnum))
+            buffer.bufnum  = bufnum
+
+            if isNil(buffer.snd_buf):
+                return false
+
+            buffer.length     = int(get_frames_buffer_SC(buffer.snd_buf))
+            buffer.size       = int(get_samples_buffer_SC(buffer.snd_buf))
+            buffer.chans      = int(get_channels_buffer_SC(buffer.snd_buf))
+            buffer.samplerate = float(get_samplerate_buffer_SC(buffer.snd_buf))
+    #scsynth
+    else:
+        #Update buffer pointer only with a new buffer number as input
+        if buffer.bufnum != bufnum:
+            buffer.snd_buf = get_buffer_SC(buffer.sc_world, cfloat(bufnum))
+            buffer.bufnum  = bufnum
+            
+            if not isNil(buffer.snd_buf):
+                buffer.length     = int(get_frames_buffer_SC(buffer.snd_buf))
+                buffer.size       = int(get_samples_buffer_SC(buffer.snd_buf))
+                buffer.chans      = int(get_channels_buffer_SC(buffer.snd_buf))
+                buffer.samplerate = float(get_samplerate_buffer_SC(buffer.snd_buf))
+
+        if isNil(buffer.snd_buf):
+            return false
     
     return true
 
 #Supernova unlocking
 when defined(multithreadBuffers):
     proc unlock_buffer*(buffer : Buffer) : void {.inline.} =
-        unlock_buffer_SC(cast[pointer](buffer.snd_buf))
+        #This check is needed as buffers could be unlocked when another one has been failed to acquire!
+        if not isNil(buffer.snd_buf):
+            unlock_buffer_SC(buffer.snd_buf)
 
 ##########
 # GETTER #
@@ -130,9 +173,29 @@ when defined(multithreadBuffers):
 proc `[]`*[I : SomeNumber](a : Buffer, i : I) : float {.inline.} =
     return float(get_float_value_buffer_SC(a.snd_buf, clong(i), clong(0)))
 
-#more than 1 channel
+#more than 1 channel (i1 == channel, i2 == index)
 proc `[]`*[I1 : SomeNumber, I2 : SomeNumber](a : Buffer, i1 : I1, i2 : I2) : float {.inline.} =
-    return float(get_float_value_buffer_SC(a.snd_buf, clong(i1), clong(i2)))
+    return float(get_float_value_buffer_SC(a.snd_buf, clong(i2), clong(i1)))
+
+#linear interp read (1 channel)
+proc read*[I : SomeNumber](buffer : Buffer, index : I) : float {.inline.} =
+    let 
+        buf_len = buffer.length
+        index1 = safemod(int(index), buf_len)
+        index2 = safemod(index1 + 1, buf_len)
+        frac : float  = float(index) - float(index1)
+    
+    return linear_interp(frac, buffer[index1], buffer[index2])
+
+#linear interp read (more than 1 channel) (i1 == channel, i2 == index)
+proc read*[I1 : SomeNumber, I2 : SomeNumber](buffer : Buffer, chan : I1, index : I2) : float {.inline.} =
+    let 
+        buf_len = buffer.length
+        index1 = safemod(int(index), buf_len)
+        index2 = safemod(index1 + 1, buf_len)
+        frac : float  = float(index) - float(index1)
+    
+    return linear_interp(frac, buffer[chan, index1], buffer[chan, index2])
 
 ##########
 # SETTER #
@@ -142,9 +205,9 @@ proc `[]`*[I1 : SomeNumber, I2 : SomeNumber](a : Buffer, i1 : I1, i2 : I2) : flo
 proc `[]=`*[I : SomeNumber, S : SomeNumber](a : Buffer, i : I, x : S) : void {.inline.} =
     set_float_value_buffer_SC(a.snd_buf, cfloat(x), clong(i), clong(0))
 
-#more than 1 channel
+#more than 1 channel (i1 == channel, i2 == index)
 proc `[]=`*[I1 : SomeNumber, I2 : SomeNumber, S : SomeNumber](a : Buffer, i1 : I1, i2 : I2, x : S) : void {.inline.} =
-    set_float_value_buffer_SC(a.snd_buf, cfloat(x), clong(i1), clong(i2))
+    set_float_value_buffer_SC(a.snd_buf, cfloat(x), clong(i2), clong(i1))
 
 #########
 # INFOS #
@@ -152,20 +215,20 @@ proc `[]=`*[I1 : SomeNumber, I2 : SomeNumber, S : SomeNumber](a : Buffer, i1 : I
 
 #length of each frame in buffer
 proc len*(buffer : Buffer) : int {.inline.} =
-    return int(get_frames_buffer_SC(buffer.snd_buf))
+    return buffer.length
 
 #Returns total size (snd_buf->samples)
 proc size*(buffer : Buffer) : int {.inline.} =
-    return int(get_samples_buffer_SC(buffer.snd_buf))
+    return buffer.size
 
 #Number of channels
-proc nchans*(buffer : Buffer) : int {.inline.} =
-    return int(get_channels_buffer_SC(buffer.snd_buf))
+proc chans*(buffer : Buffer) : int {.inline.} =
+    return buffer.chans
 
 #Samplerate (float64)
 proc samplerate*(buffer : Buffer) : float {.inline.} =
-    return float(get_samplerate_buffer_SC(buffer.snd_buf))
+    return buffer.samplerate
 
 #Sampledur (Float64)
-proc sampledur*(buffer : Buffer) : float {.inline.} =
-    return float(get_sampledur_buffer_SC(buffer.snd_buf))
+#[ proc sampledur*(buffer : Buffer) : float {.inline.} =
+    return buffer.sampledur ]#
